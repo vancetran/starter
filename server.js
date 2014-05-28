c663f87ff09ec6f5b8ffa6f360e0c5abb735cf60
Vance Tran <v@vancetran.com>
2014-05-28 10:56:27 -0700

    Server will auto open the dev site now

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var connect = require('connect'),
+    open = require('open'),
 	port = process.argv[2] || 8080;
 
 connect().use(connect.static(__dirname+"/app")).listen(port);
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+open('http://localhost:' + port);
